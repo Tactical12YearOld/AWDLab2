6462b3e9f67c62efe000aafa5543767a4635f54c
Tactical12YearOld <bschaeffer18@gmail.com>
2020-09-09 04:04:57 +0000

    modified:   app_server/routes/index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,10 +4,6 @@ var ctrlHome = require('../controllers/home');
 var crtlBlog = require('../controllers/blog')
 /* GET home page. */
 router.get('/', ctrlHome.home);
-<<<<<<< HEAD
-router.get('/blog', ctrlBlog.blog);
-=======
 //router.get('/blog', ctrlBlog.blog);
->>>>>>> 8dae91090151571492a8f8c79f6f7bd686b25065
 
 module.exports = router;
