9b1b60553b047b42217c417353a77277cf2d23ee
Tactical12YearOld <Luther199595!>
2020-09-15 19:07:22 -0400

    cont edit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,8 @@ const ctrlHome = require('../controllers/home');
 const ctrlBlog = require('../controllers/blog')
 /* GET home page. */
 router.get('/', ctrlHome.home);
-router.get('/blog', ctrlBlog.blogList);
+router.get('/blogList', ctrlBlog.blogList);
+router.get('/blogAdd', ctrlBlog.blogAdd);
 //router.get('/blog', ctrlBlog.blog);
 
 module.exports = router;
