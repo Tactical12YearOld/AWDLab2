f6e030e6af42618f9915d044770e63f734b4e0b7
Tactical12YearOld <Luther199595!>
2020-09-15 18:49:58 -0400

    cont edit

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -2,6 +2,6 @@
 module.exports.blogList = function(req, res){
   res.render('blogList');
 };
-module.exports.blogAdd = function(req res){
+module.exports.blogAdd = function(req, res){
 	res.render('blodAdd');
 };
