65cdc9556a136decbcd130a594a407cfa9ddffa0
Tactical12YearOld <Luther199595!>
2020-09-15 18:14:17 -0400

    cont edit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var ctrlHome = require('../controllers/home');
+const ctrlHome = require('../controllers/home');
 var crtlBlog = require('../controllers/blog')
 /* GET home page. */
 router.get('/', ctrlHome.home);
+router.get('/blog', ctrlBlog.blog);
 //router.get('/blog', ctrlBlog.blog);
 
 module.exports = router;
