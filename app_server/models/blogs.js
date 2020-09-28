29457043f9c796a5f2ca75bb981d8ab7065b267a
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-09-28 00:46:46 -0400

    added schema to the database

---
+++ app_server/models/blogs.js
@@ -0,0 +1,6 @@
+var mongoose = require( 'mongoose' );
+var blogSchema = new mongoose.Schema({ 
+    blogTitle: {type: String, "default": "Untitled", required: true},
+    blogText: {type: String, "default": "Empty Blog", required: true},
+    dateCreated: {type: Date,"default": Date.now}
+});
--- app_server/models/db.js
+++ app_server/models/db.js
@@ -14,6 +14,16 @@ mongoose.connection.on('error', function (err) {
 mongoose.connection.on('disconnected', function () {
     console.log('Mongoose disconnected');
 });
+var readLine = require ("readline");
+if (process.platform === win32){
+    var r1 = readLine.createInerface ({
+        input: process.stdin,
+        output: process.stdout
+    });
+    r1.on ("SIGINT", function (){
+        process.emit ("SIGINT");
+    });
+}
 // Closes (disconnects) from Mongoose DB upon shutdown
 gracefulShutdown = function (msg, callback) {
     mongoose.connection.close(function () {
@@ -32,3 +42,4 @@ process.on('SIGINT', function() {
     gracefulShutdown('app termination', function () {
       process.exit(0);
   }); });
+require('./blogs');
