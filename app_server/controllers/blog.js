23b68fea00348dbdb580be75c2746c35bfd7becf
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-09-28 02:48:40 -0400

    debugging date

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -1,18 +1,30 @@
 /* GET blog page */
+var todaysDate = new Date();
+
+function convertDate(date) {
+  var yyyy = date.getFullYear().toString();
+  var mm = (date.getMonth()+1).toString();
+  var dd  = date.getDate().toString();
+
+  var mmChars = mm.split('');
+  var ddChars = dd.split('');
+
+  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+}
 module.exports.blogList = function(req, res){
   res.render('blogList', {
     blogEntries: [{
       blogTitle: 'Title1',
       blogText: 'Text1',
-      dateCreated: Date.now()
+      dateCreated: todaysDate
     },{
       blogTitle: 'Title2',
       blogText: 'Text2',
-      dateCreated: Date.now()
+      dateCreated: todaysDate
     },{
       blogTitle: 'Title3',
       blogText: 'Text3',
-      dateCreated: Date.now()
+      dateCreated: todaysDate
     }]
   });
 };
