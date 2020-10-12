d6ca9502a59ef9cdf81d43c7d2e0a245c3d04b46
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-11 21:22:19 -0400

    edit date format

--- app_api/controllers/blogs.js
+++ app_api/controllers/blogs.js
@@ -14,14 +14,20 @@ function convertDate(date) {
   var mm = (date.getMonth()+1).toString();
   var dd  = date.getDate().toString();
   var hh = date.getHours().toString();
-  var minutes = date.getMinutes().toString();
-  
+  var m = date.getMinutes().toString();
+  var ss = date.getSeconds().toString();
+  var mmm = date.getMilliseconds().toString();
+
   var mmChars = mm.split('');
   var ddChars = dd.split('');
   var hhChars = hh.split('');
-  var minutesChars = minutes.split('');
+  var mChars = m.split('');
+  var ssChars = ss.split('');
+  var mmmChars = mmm.split('');
 
-  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]) 
+         + 'T' + (hhChars[1]?hh:"0"+hhChars[0]) + ':' + (mChars[1]?m:"0"+mChars[0]) 
+         + ':' + (ssChars[1]?ss:"0"+ssChars[0]) + ':' + (mmmChars[1]?mmm:"0"+mmmChars[0]) + 'Z';
 }
 module.exports.blogList = function(req, res){
   res.render('blogList', {
