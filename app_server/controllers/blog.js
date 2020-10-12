300f2f03f91a29ec4f30e5f6f509497454eadf0e
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-11 22:37:15 -0400

    adding request

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -1,3 +1,11 @@
+var request = require('request');
+var apiOptions = { 
+  server : "http://127.0.0.1:27017"
+};
+if (ProcessingInstruction.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://54.89.191.207/";
+}
+
 /* GET blog page */
 var todaysDate = new Date();
 
@@ -5,11 +13,21 @@ function convertDate(date) {
   var yyyy = date.getFullYear().toString();
   var mm = (date.getMonth()+1).toString();
   var dd  = date.getDate().toString();
+  var hh = date.getHours().toString();
+  var m = date.getMinutes().toString();
+  var ss = date.getSeconds().toString();
+  var mmm = date.getMilliseconds().toString();
 
   var mmChars = mm.split('');
   var ddChars = dd.split('');
+  var hhChars = hh.split('');
+  var mChars = m.split('');
+  var ssChars = ss.split('');
+  var mmmChars = mmm.split('');
 
-  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+  return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]) 
+         + 'T' + (hhChars[1]?hh:"0"+hhChars[0]) + ':' + (mChars[1]?m:"0"+mChars[0]) 
+         + ':' + (ssChars[1]?ss:"0"+ssChars[0]) + ':' + (mmmChars[1]?mmm:"0"+mmmChars[0]) + 'Z';
 }
 module.exports.blogList = function(req, res){
   res.render('blogList', {
