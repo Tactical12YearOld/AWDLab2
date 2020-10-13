2fd517294e7306cbf5b1faa819828d9de80a50dd
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-13 18:50:44 -0400

    edit blog.js

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -48,7 +48,21 @@ var renderListpage = function(req, res, responseBody){
   if (!(responseBody instanceof Array)) {
     console.log("Failed if 1");
     message = "API lookup error";
-    responseBody = [];
+    responseBody = [
+      blogEntries: [{
+        blogTitle: 'Title1',
+        blogText: 'Text1',
+        dateCreated: convertDate(todaysDate)
+      },{
+        blogTitle: 'Title2',
+        blogText: 'Text2',
+        dateCreated: convertDate(todaysDate)
+      },{
+        blogTitle: 'Title3',
+        blogText: 'Text3',
+        dateCreated: convertDate(todaysDate)
+      }]
+    ];
   } else {
     if (!responseBody.length) {
       console.log("Failed if 2");
