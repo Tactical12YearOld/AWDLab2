8c2d514e14cb40b3dad3efedd7be57031750c63a
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-13 18:38:10 -0400

    edit

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -26,7 +26,7 @@ function convertDate(date) {
          + 'T' + (hhChars[1]?hh:"0"+hhChars[0]) + ':' + (mChars[1]?m:"0"+mChars[0]) 
          + ':' + (ssChars[1]?ss:"0"+ssChars[0]) + ':' + (mmmChars[1]?mmm:"0"+mmmChars[0]) + 'Z';
 };
-/*mine
+//mine
 module.exports.blogList = function(req, res){
   var requestOptions, path;
   path = '/api/blogs';
@@ -58,8 +58,8 @@ var renderListpage = function(req, res, responseBody){
   res.render('blogList', {
     blogs: responseBody
   });
-}*/
-//his
+}
+///his
 /*module.exports.list = function(req, res){
   var requestOptions, path;
   path = '/api/blogs';
