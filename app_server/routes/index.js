40c396271bbd69757750aa2e0cffe162c09961bf
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-15 16:56:04 -0400

    edit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -8,7 +8,7 @@ router.get('/blog/:blogid', ctrlBlog.blogList);
 router.get('/blog-list', ctrlBlog.blogList);
 router.get('/blogAdd', ctrlBlog.blogAdd);
 router.get('/blogEdit', ctrlBlog.blogEdit);
-router.delete('/blogDelete', ctrlBlog.blogDelete);
+router.get('/blogDelete', ctrlBlog.blogDelete);
 router.delete('/blogDelete/:blogid', ctrlBlog.blogDelete);
 
 module.exports = router;
