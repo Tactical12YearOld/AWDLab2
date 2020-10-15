c80e3c0713bd460ca065385b51b414ff9bb1c634
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-15 17:52:35 -0400

    edit

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -85,7 +85,7 @@ var renderDeletepage = function(req, res, responseBody){
 };
 module.exports.blogDelete = function(req, res){
   var requestOptions, path;
-  path = '/api/blogs/';
+  path = '/api/blogs/blogid';
   requestOptions = {
     url: apiOptions.server + path,
     method : "DELETE",
