323e9fc4a982948150bc400e7b67e83362c12a21
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-15 15:41:50 -0400

    edit api model

--- app_api/models/blogs.js
+++ app_api/models/blogs.js
@@ -1,10 +1,10 @@
 var mongoose = require( 'mongoose' );
 var blogSchema = new mongoose.Schema({ 
-    
+    blog:{
     blogTitle: {type: String, default: "Untitled", required: true},
     blogText: {type: String, default: "Empty Blog", required: true},
-    _id: {type: String, default: blogs.str, required: true},
+    _id: {type: String, default: blogid.blogSchema.Objectid(), required: true},
     dateCreated: {type: Date,default: Date.now, required: true}
-});
+}});
 //compiling schema into models
 mongoose.model('blogs', blogSchema);
