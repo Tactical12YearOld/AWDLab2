8d743bf43a564c7a25429b987d365e8f3022502e
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-15 15:31:01 -0400

    edit

--- app_server/models/blogs.js
+++ app_server/models/blogs.js
@@ -1,6 +1,7 @@
 var mongoose = require( 'mongoose' );
 
 var blogSchema = new mongoose.Schema({ 
+    
     blogTitle: {type: String, default: "Untitled", required: false},
     blogText: {type: String, default: "Empty Blog", required: false},
     _id: {type: String, ObjectId},
