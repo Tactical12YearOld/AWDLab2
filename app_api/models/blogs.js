7ad9e6d72881728dd2007a87a51303eab8c47507
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-15 15:32:31 -0400

    eidt

--- app_api/models/blogs.js
+++ app_api/models/blogs.js
@@ -3,6 +3,7 @@ var blogSchema = new mongoose.Schema({
     
     blogTitle: {type: String, "default": "Untitled", required: true},
     blogText: {type: String, "default": "Empty Blog", required: true},
+    _id: {type: String, required: true},
     dateCreated: {type: Date,"default": Date.now, required: true}
 });
 //compiling schema into models
