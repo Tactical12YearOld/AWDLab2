82c0038937fdd9f5e6bc59f8c83497a3c0fc7679
Ben Schaeffer <benschaeffer@Bens-MacBook-Pro.local>
2020-10-16 00:32:00 -0400

    edit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,6 @@ const ctrlHome = require('../controllers/home');
 const ctrlBlog = require('../controllers/blog')
 /* GET home page. */
 router.get('/', ctrlHome.home);
-router.get('/blog-list/:blogid', ctrlBlog.blogList);
 router.get('/blog-list/', ctrlBlog.blogList);
 router.get('/blog-single/:blogid', ctrlBlog.blogShowOne);
 router.get('/blogAdd', ctrlBlog.blogAdd);
