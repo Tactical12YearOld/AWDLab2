06834ba6d94821b96126d7fd8fe29476eb0d53ad
Ben Schaeffer <benschaeffer@Bens-MacBook-Pro.local>
2020-10-22 00:00:41 -0400

    edit

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -81,7 +81,7 @@ module.exports.doBlogAdd = function(req, res){
   console.log("bout to make post struct")
   POSTDATA = {  
     blogTitle: req.body.blogTitle,
-    blogText: req.body.blogText,
+    blogText: req.body.blogText
   };
   console.log("Building request options");
   requestOptions = {
