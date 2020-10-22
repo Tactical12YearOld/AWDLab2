5116813cdbddad2b750d48392ef59c53cdefc31a
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-22 17:35:58 -0400

    edit

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -6,7 +6,7 @@ var apiOptions = {
 /* GET blog page */
 var todaysDate = new Date();
 
-function convertDate(date) {
+var convertDate = function (date) {
   var yyyy = date.getFullYear().toString();
   var mm = (date.getMonth()+1).toString();
   var dd  = date.getDate().toString();
@@ -21,7 +21,10 @@ function convertDate(date) {
   var mChars = m.split('');
   var ssChars = ss.split('');
   var mmmChars = mmm.split('');
-
+  if (mm ==='10')
+  {
+    mm = "October";
+  }
   return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]) 
          + 'T' + (hhChars[1]?hh:"0"+hhChars[0]) + ':' + (mChars[1]?m:"0"+mChars[0]) 
          + ':' + (ssChars[1]?ss:"0"+ssChars[0]) + ':' + (mmmChars[1]?mmm:"0"+mmmChars[0]) + 'Z';
