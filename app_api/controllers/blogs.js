d17bde0689ce1467cc772b27579e7fcedc4b3371
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-10-22 11:47:01 -0400

    edit

--- app_api/controllers/blogs.js
+++ app_api/controllers/blogs.js
@@ -26,9 +26,9 @@ module.exports.blogList = function(req, res){
 
 module.exports.blogReadOne = function(req, res){
     console.log('Finding blog details API', req.params);
-    if (req.params)
+    if (req.params && req.params.blogid)
     {
-        blogModel.findById(req.params)
+        blogModel.findById(req.params.blogid)
                  .exec(function(err, blogs) 
         {
           if (!blogs) 
