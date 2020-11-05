15d947ce050f23d4dbb28af1b777459ea9e9d107
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-11-05 17:25:51 -0500

    edit

--- app.js
+++ app.js
@@ -21,9 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 
 app.use('/', routes);
 app.use('/api', routesApi);
+app.use(function(req, res){
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- app_client/bloggerApp.js
+++ app_client/bloggerApp.js
@@ -1,5 +1,5 @@
 
-var app = angular.module('bloggerApp', ['ngRoute', 'ui.router']);
+var app = angular.module('bloggerApp', ['ngRoute']);
 
 /* ROUTER PROVIDER */
 app.config(function($routeProvider) {
