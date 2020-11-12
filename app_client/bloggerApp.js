9de87bfcd434d930b051e2d91a95dc9dfc5fd123
Ben Schaeffer <bschaeffer18@gmail.comgit config --global user.name Tactical12YearOld>
2020-11-12 18:46:41 -0500

    edit

--- app_client/bloggerApp.js
+++ app_client/bloggerApp.js
@@ -69,8 +69,6 @@ app.controller('HomeController', function HomeController() {
 app.controller('ListController', [ '$http', function ListController($http) {
    console.log("im in the list controller.");
    var vm = this;
-   vm.title = "Ben Schaeffer's Blog";
-   vm.message = "Welcome to my blog!!!";
     vm.title = "Blog List";
     getAllBlogs($http)
         .then(
